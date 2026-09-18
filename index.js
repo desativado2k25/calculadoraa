43215f0c93053087b029dabc6e9b5f50d3a4bf71
desativado2k25 <gustavo.da.macedo@escola.pr.gov.br>
2026-09-18 07:58:05 -0300

    Add initial HTML structure for calculator

---
+++ index.js
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+
+
+
+
+<head>
+
+
+    <meta charset="UTF-8">
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>Document</title>
+
+
+    <link rel="stylesheet" href="css/index.css">
+
+
+</head>
+
+
+
+
+
+<body>
+
+
+    <div id=card>
+
+
+        <div id="titulo">
+
+
+            <h1>Minha Calculadora</h1>
+
+
+        </div>
+
+
+        <div id="input">
+
+
+            <input type="number" placeholder="Primeiro Numero">
+
+
+            <input type="number" placeholder="Segundo Numero">
+
+
+        </div>
+
+
+        <div>
+
+
+            <input type="submit" value="+" id="somar" onclick= "somar()">
+
+
+            <input type="submit" value="-" id="subtrair" onclick= "subtrair()">
+
+
+            <input type="submit" value="x" id="multiplicar" onclick= "multiplicar()">
+
+
+            <input type="submit" value="&#247" id="dividir" onclick= "dividir()">
+
+
+        </div>
+
+
+        <div>
+
+
+            <p>Resultado:</p>
+
+
+        </div>
+
+
+    </div>
+
+
+</body>
+
+
+
+
+
+</html>
